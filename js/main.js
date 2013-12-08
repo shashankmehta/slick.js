d540b5b9f760aa0d38a5af3b20992bef15c0370e
Shashank Mehta <me@shashankmehta.in>
2013-12-08 22:10:04 +0530

    Shifted to slick.config for settings. Implemented serial source.

--- js/main.js
+++ js/main.js
@@ -1,39 +1,61 @@
-$(document).ready(function(){
+(function($, root, undefined){
+
 	slick = {};
 
 	slick.state = {
 		current: 0,
 		start: 0,
-		end: 36
-	}
+		end: 0,
+
+		slide: {
+			current:0,
+			total: 0,
+		}
+	};
 
 	slick.hooks = {
 		next: function(){
 			if(slick.state.current < slick.state.end){
-				slick.state.current++;
-				$('.slick-content').append('<img src="/slide/out-'+slick.state.current+'.jpg" class="loader">');
+				if(slick.config.serial_source === true){
+					var step = ++slick.state.current;
+				}
+				$('.slick-content').append('<img src="'+ slick.hooks.imagePath(step) +'" class="loader">');
 				$('.slick-content img.loader').load(function(){
 					$('.slick-content img.current').remove();
 					$('.slick-content img.loader').addClass('current').removeClass('loader');
 				})
-				$('.slick .current-no').html(slick.state.current+1);
+				$('.slick .current-no').html(++slick.state.slide.current);
 			}
 		},
 
 		back: function(){
 			if(slick.state.current > slick.state.start){
-				slick.state.current--;
-				$('.slick-content').append('<img src="/slide/out-'+slick.state.current+'.jpg" class="loader">');
+				if(slick.config.serial_source === true){
+					var step = --slick.state.current;
+				}
+				$('.slick-content').append('<img src="'+ slick.hooks.imagePath(step) +'" class="loader">');
 				$('.slick-content img.loader').load(function(){
 					$('.slick-content img.current').remove();
 					$('.slick-content img.loader').addClass('current').removeClass('loader');
 				})
-				$('.slick .current-no').html(slick.state.current+1);
+				$('.slick .current-no').html(--slick.state.slide.current);
 			}
+		},
+
+		imagePath: function(step){
+			var parts = slick.config.source.split('*');
+			return parts[0] + step + parts[1];
 		}
-	}
+	};
 
 	slick.init = function(){
+		if(slick.config.serial_source === true){
+			if(typeof slick.config.source === "string"){
+				slick.state.current = slick.config.start - 1;
+				slick.state.end = slick.config.end;
+				slick.hooks.next();
+			}
+		}
 		$('.slick-controls .next').click(function(){
 			slick.hooks.next();
 		});
@@ -42,9 +64,7 @@ $(document).ready(function(){
 		});
 		$('.slick .current-no').html(slick.state.current+1);
 		$('.slick .total').html(slick.state.end+1);
-	}
-
-	slick.init();
+	};
 
 	$(document).keyup(function(e) {
 		if (e.keyCode ===  39 || e.keyCode ===  40) {
@@ -54,4 +74,4 @@ $(document).ready(function(){
 			slick.hooks.back();
 		}
 	});
-});
+}(jQuery, window));
