1baeba10dc1eff0113504d3017c7c7c6308d0df8
Shashank Mehta <me@shashankmehta.in>
2013-12-11 23:31:05 +0530

    v0.9.0. Now styling of default theme is left

--- README.md
+++ README.md
@@ -8,26 +8,44 @@ We followed the SpeakerDeck format. User uploads presentations in PDF format whi
 
 To create the presentation, include jQuery and slick.js
 
-    var s = new Slick('.slick', options);
+```javascript
+var s = new Slick('.slick', options); // .slick is class/id of div to be used as container for Slick.
+```
 
-The options available are (with defaults shown as set):
+The options available are:
 
-    options = {
-	    source: undefined, // eg '/slide/slick-*.jpg',
-	    serialSource: false, // true | false
-	    start: undefined, // eg 0
-	    end: undefined, // eg 5
-	    next: '.next', // class/id of element to which Slick should assign next
-	    prev: '.prev', // class/id of element to which Slick should assign prev
-	    keyControl: true, // In case of multiple presentations, set true for keyboard control on this one
-	    container: '.slick' // class/id of div to be used as container for Slick.
-    }
+```javascript
+// Minimum options needed with default theme
+options = {
+	source: 'slide/slick-*.jpg',
+	start: 0,
+	end: 5,
+};
+
+// All options available with default values shown as set
+allOptions = {
+	source: undefined, // eg '/slide/slick-*.jpg'
+	start: undefined, // eg 0
+	end: undefined, // eg 5
+	keyControl: true, // In case of multiple presentations, set true for keyboard control on this one
+	theme: {
+	    content: '.slick-content', // class/id for div where images will be loaded
+	    currentNo: '.current-no', // class/id for showing current slide no
+	    totalNo: '.total', // class/id for showing total number of slides
+	    next: '.next', // class/id for the 'next' button
+	    prev: '.prev', // class/id for the 'prev' button
+	}
+}
+```
 
 Programmatic access available:
-	
-    // s is instance of Slick
-    Slick.next(s);
-    Slick.prev(s);
+
+```javascript	
+// s is an instance of Slick
+Slick.next(s);
+Slick.prev(s);
+Slick.skip(s, 6); // 6 is slide number
+```
 
 ##Features
 
@@ -36,12 +54,13 @@ Programmatic access available:
 3. Quick Skipping: If the user quickly clicks on next, say, 5 times, the intermediate slides are not loaded.
 4. The slide is not changed until it loads completely. _(This is not a gallery plugin)_
 5. The slides are not deleted but kept hidden in DOM. _(unlike SpeakerDeck)_
+6. Slick supports theming of presentation player.
 
-Slick will support theming of presentation player. The default options are set for the theme used in the repository. The CSS is in `examples/css/style.css` and HTML in `examples/index.html`.
+The default options are set for the theme used in the repository. The CSS is in `examples/css/style.css` and HTML in `examples/index.html`.
+
+Fullscreen option is available in default theme. However it is not part of slick.js due to browser quirks in styling fullscreen elements. Take a look at source of default theme in case you want to add it to your theme. We are using @sindresorhus' [screenfull.js](https://github.com/sindresorhus/screenfull.js) to manage cross browser compatibility. 
 
 ##To Do
 
 1. Add sample PDF to image conversion scripts as well as instructions for PowerPoint and KeyNote
-2. Add fullscreen support
-3. Add support for theming.
-4. Add support for serial no indicator be set to anything, not just *
+2. Add support for serial no indicator be set to anything, not just *
--- dist/slick.js
+++ dist/slick.js
@@ -1,6 +1,6 @@
 /*!
 * slick.js
-* v0.5.0 - 2013-12-11
+* v0.9.0 - 2013-12-11
 * https://github.com/shashankmehta/slick.js
 * (c) Shashank Mehta; MIT License
 */
@@ -17,21 +17,33 @@
             source: undefined,
             start: undefined,
             end: undefined,
-            next: '.next',
-            prev: '.prev',
-            contentClass: '.slick-content',
             keyControl: true,
-            container: container,
-            content: undefined
+            content: undefined,
+            theme: {
+                container: container,
+                content: '.slick-content',
+                currentNo: '.current-no',
+                totalNo: '.total',
+                next: '.next',
+                prev: '.prev',
+            }
         };
 
         for (var option in this.options) {
-            if (this.options.hasOwnProperty(option)) {
+            if (this.options.hasOwnProperty(option) && option !== 'theme') {
                 this.options[option] = config[option] !== undefined ? config[option] : this.options[option];
             }
         }
 
-        this.options.content = this.options.container + ' ' + this.options.contentClass;
+        if(config.theme !== undefined){
+            for (var val in this.options.theme) {
+                if (this.options.theme.hasOwnProperty(val)) {
+                    this.options.theme[val] = config.theme[val] !== undefined ? config.theme[val] : this.options.theme[val];
+                }
+            }
+        }
+
+        this.options.content = this.options.theme.container + ' ' + this.options.theme.content;
 
         this.state = {
             // Stores slide url no that is visible
@@ -39,7 +51,6 @@
             
             start: this.options.start,
             end: this.options.end,
-            getNext: false,
 
             // Stores values that is shown in controls
             slide: {
@@ -60,14 +71,9 @@
         // Main function for handling next/forwarding of slides
         next: function() {
             var slick = this;
-            var container = slick.options.container;
 
             if(slick.state.slide.current < slick.state.slide.total){
-
-                // Setting and displaying the current slide no
-                $(container + ' .current-no').html(++slick.state.slide.current);
-
-                var step = slick.state.slide.current + slick.state.slide.difference;
+                var step = ++slick.state.slide.current + slick.state.slide.difference;
                 slick.hooks.setSlide.apply(this, [step]);
             }
         },
@@ -75,14 +81,9 @@
         // Main function for handling going backward
         prev: function(){
             var slick = this;
-            var container = slick.options.container;
 
             if(slick.state.slide.current > 1){
-
-                // Setting and displaying the current slide no
-                $(container + ' .current-no').html(--slick.state.slide.current);
-
-                var step = slick.state.slide.current + slick.state.slide.difference;
+                var step = --slick.state.slide.current + slick.state.slide.difference;
                 slick.hooks.setSlide.apply(this, [step]);
             }
         },
@@ -90,6 +91,15 @@
         setSlide: function(step){
             var slick = this;
             var slideStatus = slick.hooks.slideStatus.apply(slick, [step]);
+            slick.state.slide.current = step - slick.state.slide.difference;
+
+            if($(slick.options.theme.container + ' .skip' + slick.options.theme.currentNo).is(':input')){
+                $(slick.options.theme.container + ' ' + slick.options.theme.currentNo).val(slick.state.slide.current);
+            }
+            else {
+                $(slick.options.theme.container + ' .skip').val(slick.state.slide.current);
+                $(slick.options.theme.container + ' ' + slick.options.theme.currentNo).html(slick.state.slide.current);
+            }
             
             if(slideStatus === 1){
                 $(slick.options.content + ' img.current').removeClass('current').addClass('cached-slide').hide();
@@ -157,8 +167,14 @@
             return parts[0] + step + parts[1];
         },
 
-        check: function(){
-            console.log(this);
+        skip: function(val){
+            if($.isNumeric(val)){
+                var slick = this;
+
+                var step = parseInt(val) + slick.state.slide.difference;
+                slick.hooks.getSlide.apply(slick, [step]);
+                slick.hooks.setSlide.apply(slick, [step]);
+            }
         }
 
     };
@@ -172,15 +188,15 @@
         }
 
         // Attaches event listeners for next/prev buttons
-        $(slick.options.container + ' ' + slick.options.next).click(function(e){
+        $(slick.options.theme.container + ' ' + slick.options.theme.next).click(function(e){
             e.preventDefault();
             slick.hooks.next.apply(slick);
         });
-        $(slick.options.container + ' ' + slick.options.prev).click(function(e){
+        $(slick.options.theme.container + ' ' + slick.options.theme.prev).click(function(e){
             e.preventDefault();
             slick.hooks.prev.apply(slick);
         });
-        $(slick.options.container + ' .total').html(slick.state.end - slick.state.start + 1);
+        $(slick.options.theme.container + ' ' + slick.options.theme.totalNo).html(slick.state.end - slick.state.start + 1);
 
         // Ataches keyboard control
         if(slick.options.keyControl){
@@ -193,10 +209,18 @@
                 }
             });
         }
+        
+        $(slick.options.theme.container + ' .skip').keypress(function(e){
+            if(e.keyCode === 13){
+                slick.hooks.skip.apply(slick, [$(this).val()]);
+                $(this).blur();
+            }
+        });
     };
 
     window.Slick = Slick;
 
+    // Exposing programmatic access
     Slick.next = function(slick){
         if(slick.constructor === Slick){
             slick.hooks.next.apply(slick);
@@ -206,6 +230,12 @@
     Slick.prev = function(slick){
         if(slick.constructor === Slick){
             slick.hooks.prev.apply(slick);
+        }
+    };
+
+    Slick.skip = function(slick, step){
+        if(slick.constructor === Slick){
+            slick.hooks.skip.apply(slick, [step]);
         }
     };
 
