0a95844a92f8854e88bee289abeb6b1e0d9c23a7
Shashank Mehta <me@shashankmehta.in>
2013-12-20 20:31:50 +0530

    Slide indicator length doesn't exceed limit anymore

--- src/slick.js
+++ src/slick.js
@@ -99,12 +99,12 @@
                 $(slick.options.theme.container + ' ' + slick.options.theme.currentNo).html(slick.state.slide.current);
             }
 
-            $(slick.options.theme.container + ' .length').width(100 * slick.state.slide.current / slick.state.slide.total + '%');
             
             if(slideStatus === 1){
                 $(slick.options.content + ' img.current').removeClass('current').addClass('cached-slide').hide();
                 $(slick.options.content + ' img[data-slide=' + step + ']').removeClass('cached-slide').addClass('current').show();
                 slick.state.current = step;
+                $(slick.options.theme.container + ' .length').width(100 * slick.state.slide.current / slick.state.slide.total + '%');
                 slick.hooks.getSlide.apply(slick, [step+1]);
                 return;
             }
@@ -137,6 +137,7 @@
                 $(slick.options.content + ' img.current').removeClass('current').addClass('cached-slide').hide();
                 $(this).removeClass('cached-slide').addClass('current').show();
                 slick.state.current = step;
+                $(slick.options.theme.container + ' .length').width(100 * (step + 1) / slick.state.slide.total + '%');
                 slick.hooks.getSlide.apply(slick, [step+1]);
             });
         },
